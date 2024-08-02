99cdda72949d6a82957320702157d96090932b5b
AjitGode <godeajit71@gmail.com>
2024-08-02 17:41:33 +0530

    Update about sec

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,7 @@ const About = ()=>{
         <div className="about-content">
           <h2 className="heading">About <span>Me</span></h2>
           <h3>Frontend Developer</h3>
-          <p>Hi, I'm Ajit Gode. I have work experience as a tester as well as a frontend developer. I have done my schooling and graduation from Sangamner and completed my post-graduation in MCA from Savitribai Phule Pune University.</p>
+          <p>A dedicated QA Developer with a passion for ensuring software product quality and development. As a fresher in the industry, I specialize in automation testing and development using tools like Cypress and Playwright. I am committed to continuous learning and staying updated with the latest technologies in the field.</p>
           <a href="https://www.linkedin.com/in/ajit-gode-566495157/" target="_blank" rel="noopener noreferrer" className="btn">Read More</a>
         </div>
       </section>
