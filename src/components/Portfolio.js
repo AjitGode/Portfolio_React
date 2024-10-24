a90ff6d4b785849fe7dfb3d643976faa2c356935
AjitGode <godeajit71@gmail.com>
2024-10-24 14:50:28 +0530

    Added new portfolio project

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -9,9 +9,11 @@ const Portfolio = ()=>{
         <div className="portfolio-container">
           {[
             { img: 'portfolio1.jpg', title: 'INSTITUTE MANAGEMENT SYSTEM', desc: 'Institute Management system is designed for managing and storing all the data available in the institute. It involves login of employee. The website helps maintaining institute data like adding institute data, enrollment data, course data, user data.', link: 'https://github.com/AjitGode/AngularProject' },
+            { img: 'bg-remover.png', title: 'Bg-remover', desc: 'I have creating a Background Removal App that allows users to upload an image. Then our app will process the image and remove the background using AI and then user can download the image with a transparent background.', link: 'https://bg-remover-beige.vercel.app/' },
             { img: 'portfolio2.jpg', title: 'BLOOD BANK MANAGEMENT SYSTEM', desc: 'It is a Web Application that helps us to find blood group with its most efficient time to take care of the blood and it is easy to hand over the blood to the hospitals to help people to get rid of their problems.', link: 'https://github.com/AjitGode/BloodBank' },
-            { img: 'portfolio3.jpg', title: 'PORTFOLIO', desc: 'My Portfolio website which contains all the information about me, like my work experience, skills, contact info, and other related information.', link: 'https://github.com/AjitGode' },
-            { img: 'portfolio4.jpg', title: 'Jobseeker', desc: 'A Jobseeker website for managing data and visualizing it in a user-friendly way.', link: 'https://github.com/AjitGode/Jobseeker.git' }
+            { img: 'portfolio3.jpg', title: 'PORTFOLIO', desc: 'My Portfolio website which contains all the information about me, like my work experience, skills, contact info, and other related information.', link: 'https://ajitgode.github.io/Portfolio_React/' },
+            { img: 'portfolio4.jpg', title: 'Jobseeker', desc: 'A Jobseeker website for managing data and visualizing it in a user-friendly way.', link: 'https://github.com/AjitGode/Jobseeker.git' },
+            { img: 'bg-remover.png', title: 'Bg-remover', desc: 'I have creating a Background Removal App that allows users to upload an image. Then our app will process the image and remove the background using AI and then user can download the image with a transparent background.', link: 'https://bg-remover-beige.vercel.app/' }
           ].map((project, index) => (
             <div className="portfolio-box" key={index}>
               <div className="portfolio-img">
